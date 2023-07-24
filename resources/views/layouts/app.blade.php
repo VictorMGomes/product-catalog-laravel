0bc41ef40c6e842069ddf667b31aac827031a553
Victor M. Gomes <victorvhmgomes@gmail.com>
2023-07-23 23:22:20 -0300

    Melhoria na estilização do projeto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo de Produtos</title>
-    <!-- Add your CSS and JavaScript dependencies here -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap CSS via CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
         <div class="container">
             <a class="navbar-brand" href="{{ route('products.index') }}">Catálogo de Produtos</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,10 +23,14 @@
     </nav>
 
     <div class="container mt-4">
-        @yield('content')
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                @yield('content')
+            </div>
+        </div>
     </div>
 
-    <!-- Add your JavaScript dependencies here -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Bootstrap JavaScript via CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
